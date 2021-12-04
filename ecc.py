6405173c47e8a3204600604cfd20a63c205fa1f6
Shagun Uppal <uppalshagun18@gmail.com>
2021-12-04 20:48:22 +0530

    first commit

---
+++ ecc.py
@@ -0,0 +1,84 @@
+from tinyec import registry
+import secrets
+from Crypto.Cipher import AES
+import hashlib, secrets, binascii
+import time
+
+curve = registry.get_curve('brainpoolP256r1')
+def compress_point(point):
+    return hex(point.x) + hex(point.y % 2)[2:]
+def ecc_calc_encryption_keys(pubKey):
+    ciphertextPrivKey = secrets.randbelow(curve.field.n)
+    ciphertextPubKey = ciphertextPrivKey * curve.g
+    sharedECCKey = pubKey * ciphertextPrivKey
+    return (sharedECCKey, ciphertextPubKey)
+def ecc_calc_decryption_key(privKey, ciphertextPubKey):
+    sharedECCKey = ciphertextPubKey * privKey
+    return sharedECCKey
+
+def encrypt_AES_GCM(msg, secretKey):
+    aesCipher = AES.new(secretKey, AES.MODE_GCM)
+    ciphertext, authTag = aesCipher.encrypt_and_digest(msg)
+    return (ciphertext, aesCipher.nonce, authTag)
+
+def decrypt_AES_GCM(ciphertext, nonce, authTag, secretKey):
+    aesCipher = AES.new(secretKey, AES.MODE_GCM, nonce)
+    plaintext = aesCipher.decrypt_and_verify(ciphertext, authTag)
+    return plaintext
+
+def ecc_point_to_256_bit_key(point):
+    sha = hashlib.sha256(int.to_bytes(point.x, 32, 'big'))
+    sha.update(int.to_bytes(point.y, 32, 'big'))
+    return sha.digest()
+
+curve = registry.get_curve('brainpoolP256r1')
+
+def encrypt_ECC(msg, pubKey):
+    ciphertextPrivKey = secrets.randbelow(curve.field.n)
+    sharedECCKey = ciphertextPrivKey * pubKey
+    secretKey = ecc_point_to_256_bit_key(sharedECCKey)
+    ciphertext, nonce, authTag = encrypt_AES_GCM(msg, secretKey)
+    ciphertextPubKey = ciphertextPrivKey * curve.g
+    return (ciphertext, nonce, authTag, ciphertextPubKey)
+
+def decrypt_ECC(encryptedMsg, privKey):
+    (ciphertext, nonce, authTag, ciphertextPubKey) = encryptedMsg
+    sharedECCKey = privKey * ciphertextPubKey
+    secretKey = ecc_point_to_256_bit_key(sharedECCKey)
+    plaintext = decrypt_AES_GCM(ciphertext, nonce, authTag, secretKey)
+    return plaintext
+
+
+begin = time.time()
+privKey = secrets.randbelow(curve.field.n)
+pubKey = privKey * curve.g
+print("private key:", hex(privKey))
+print("public key:", compress_point(pubKey))
+(encryptKey, ciphertextPubKey) = ecc_calc_encryption_keys(pubKey)
+print("ciphertext pubKey:", compress_point(ciphertextPubKey))
+print("encryption key:", compress_point(encryptKey))
+decryptKey = ecc_calc_decryption_key(privKey, ciphertextPubKey)
+print("decryption key:", compress_point(decryptKey))
+msg = b'A message for encryption' \
+      b'.'
+print("original msg:", msg)
+privKey = secrets.randbelow(curve.field.n)
+pubKey = privKey * curve.g
+
+encryptedMsg = encrypt_ECC(msg, pubKey)
+encryptedMsgObj = {
+    'ciphertext': binascii.hexlify(encryptedMsg[0]),
+    'nonce': binascii.hexlify(encryptedMsg[1]),
+    'authTag': binascii.hexlify(encryptedMsg[2]),
+    'ciphertextPubKey': hex(encryptedMsg[3].x) + hex(encryptedMsg[3].y % 2)[2:]
+}
+print("encrypted msg:", encryptedMsgObj)
+
+decryptedMsg = decrypt_ECC(encryptedMsg, privKey)
+print("decrypted msg:", decryptedMsg)
+
+
+end = time.time()
+  
+
+print(f"Total runtime of the program is {end - begin}")
---
+++ rsa.py
@@ -0,0 +1,32 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import binascii
+import time
+begin = time.time()
+ 
+keyPair = RSA.generate(3072)
+ 
+pubKey = keyPair.publickey()
+print(f"Public key:  (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
+pubKeyPEM = pubKey.exportKey()
+print(pubKeyPEM.decode('ascii'))
+ 
+print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
+privKeyPEM = keyPair.exportKey()
+print(privKeyPEM.decode('ascii'))
+ 
+#encryption
+msg = b'A message for encryption'
+encryptor = PKCS1_OAEP.new(pubKey)
+encrypted = encryptor.encrypt(msg)
+print("Encrypted:", binascii.hexlify(encrypted))
+
+#decryption
+decryptor = PKCS1_OAEP.new(keyPair)
+decrypted = decryptor.decrypt(encrypted)
+print('Decrypted:', decrypted)
+
+end = time.time()
+  
+
+print(f"Total runtime of the program is {end - begin}")
